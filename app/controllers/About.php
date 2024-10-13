8c5cf00536b8018263ca12a29de59788c41f76bf
Kasuma <113836758+kasumabalidps@users.noreply.github.com>
2024-10-13 11:07:31 +0800

    Initial commit

---
+++ app/controllers/About.php
@@ -0,0 +1,11 @@
+<?php
+
+class About {
+    public function page(){
+        echo 'About/page';
+    }
+
+    public function index(){
+        echo 'Halo, Nama Saya Nando, saya seorang mahasiswa';
+    }
+}
