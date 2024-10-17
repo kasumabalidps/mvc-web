f9d53197280bc5ecc839e642f90dd37d56fbbac4
Kasuma <113836758+kasumabalidps@users.noreply.github.com>
2024-10-17 11:06:38 +0800

    update

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman About</title>
-</head>
-<body>
-    <h1>About Me</h1>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/image.png" alt="">
     <h1>Halo, Nama Saya <?=$data['nama'] ?>, umur saya <?php $data['umur'] ?>, saya adalah seorang <?php $data['pekerjaan'] ?></h1>
-</body>
-</html>
+</div>
