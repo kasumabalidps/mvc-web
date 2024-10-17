891eb330e0b32bc3d0a56491dd55cf547c83d859
Kasuma <113836758+kasumabalidps@users.noreply.github.com>
2024-10-17 09:39:33 +0800

    update

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -4,5 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman <?= $data['judul']; ?></title>
+    <link rel="stylesheet" href="http://localhost/mvc-web/public/css/bootstrap.css">
 </head>
 <body>
